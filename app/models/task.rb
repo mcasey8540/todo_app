1911f29b81dc3db9e8ba479dcf8e81d694fc3122
Michael Casey <mcasey8540@gmail.com>
2012-11-23 13:37:41 -0800

    add tag column to list show view

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,10 @@
 class Task < ActiveRecord::Base
   attr_accessible :completed, :description, :list_id, :priority, :due_at, :tag
 
-  validates :description, :length => { :minimum => 3}
+  validates :description, :length => { :in => 3..30}
   #validate :due_date_must_be_today_or_later
   validates :priority, :inclusion => { :in => %w(low high), :message => "must select high or low"}  
+
 
   belongs_to :list
 
