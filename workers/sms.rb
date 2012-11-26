c0b584bf532e04eb5c15191a62773bfdc56dee08
Michael Casey <mcasey8540@gmail.com>
2012-11-25 20:56:07 -0800

    add Iron Worker and Twilio feature in task controller

--- workers/sms.rb
+++ workers/sms.rb
@@ -3,17 +3,14 @@ require 'twilio-ruby'
 # Inputs
 sid = 'AC1714be77fe4c23e291215baead5e5b5f'
 token = 'c55f2685349b65367edb9dfba4fc142b'
-from = '+114156399417'
-to = '+16107417722'
-
-# This is where we can generate a custom message for the user!
-# Pull in data from your database, from API's, etc. and crunch it into something interesting.
-# But for this example, we'll just set it to this:
-body = "Hello from TodoApp!"
+from = '+14156399417'
+to = @params[:to]
+body = "TodoApp Reminder: '#{@params[:description]}' is due at '#{@params[:due_at]}'"
 
 
 # Initialize the Twilio client
 @client = Twilio::REST::Client.new(sid, token)
+
 
 # Send the message
 r = @client.account.sms.messages.create(
