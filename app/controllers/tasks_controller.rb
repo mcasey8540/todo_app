7bdd430292c0a00866cc0bd22dd10d8a15e8bf90
Michael Casey <mcasey8540@gmail.com>
2012-11-29 21:40:41 -0800

    refactor complete task method on Task model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
 
 	before_filter :find_list, :only => [:create, :complete, :new, :edit, :update, :delete]
-
 	respond_to :html, :xml, :js
 
 	def create
@@ -18,26 +17,8 @@ class TasksController < ApplicationController
 	end
 
 	def complete
-		@task = @list.tasks.find(params[:id])
-		if @task.completed == false
-			@task.completed = true
-			if @task.save
-				flash[:notice] = "Congrats! You've completed #{@task.description}. You should be proud!"
-				redirect_to @list
-			else
-				flash[:alert] = "Something went wrong. Try again"
-				redirect_to @list
-			end
-		else
-			@task.completed = false
-			if @task.save
-				flash[:notice] = "Looks like #{@task.description} hasn't been completed. Get crackin!"
-				redirect_to @list
-			else
-				flash[:alert] = "Something went wrong. Try again"
-				redirect_to @list
-			end
-		end
+		Task.complete_task(params[:id])
+		redirect_to @list
 	end
 
 	def new
@@ -51,6 +32,7 @@ class TasksController < ApplicationController
 	def update
 		@task = @list.tasks.find(params[:id])
 		if @task.update_attributes(params[:task])
+			schedule_sms(@task)
 			flash[:notice] = "#{@task.description} successfully updated"
 			redirect_to @list
 		else
@@ -77,23 +59,25 @@ private
 	end
 
 	def schedule_sms(task)
-		d = task.due_at.to_s
-		time = Time.new(d[0..3],d[5..6],d[8..9]) - (3600 * task.sms_reminder.to_i )
-		@iw = IronWorkerNG::Client.new
-		@iw.schedules.create("sms", 
-										{
-										description: task.description, 
-										sms_reminder: task.sms_reminder, 
-										due_at: task.due_at.to_date, 
-										to: current_user.phone_number,
-										},	
-                     {
-                         #This is the schedule
-                         :start_at => time,
-                         #:start_at => task.due_at - (3600 * task.sms_frequency.to_i),
-                         :run_times => 1,
-                         #:end_at => Time.now + 180
-                     })
+		unless task.sms_reminder.nil? 
+			d = task.due_at.to_s
+			time = Time.new(d[0..3],d[5..6],d[8..9]) - (3600 * task.sms_reminder.to_i )
+			@iw = IronWorkerNG::Client.new
+			@iw.schedules.create("sms", 
+											{
+											description: task.description, 
+											sms_reminder: task.sms_reminder, 
+											due_at: task.due_at.to_date, 
+											to: current_user.phone_number,
+											},	
+	                     {
+	                         #This is the schedule
+	                         :start_at => time,
+	                         #:start_at => task.due_at - (3600 * task.sms_frequency.to_i),
+	                         :run_times => 1,
+	                         #:end_at => Time.now + 180
+	                     })
+		end
 	end
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,30 +1,21 @@
 class Task < ActiveRecord::Base
 
-  after_validation do
-    self.sms_reminder = self.sms_reminder.gsub!(/\D/,"")
-  end
-
   attr_accessible :completed, :description, :list_id, :priority, :due_at, :tag, :sms_reminder
+  belongs_to :list
 
   validates :description, :length => { :in => 3..30}
   validates :priority, :inclusion => { :in => %w(low high), :message => "must select high or low"}
-  validates :sms_reminder, :inclusion => { :in => ["5 hours", "3 hours", "1 hour"], :message => "must SMS reminder"}
+  #validates :sms_reminder, :inclusion => { :in => ["5 hours", "3 hours", "1 hour"], :message => "must SMS reminder"}
   validates :tag, presence: :true
-  # validate :sms_frequency_must_be_before_due_at
-
-  belongs_to :list
-
-  scope :complete, where(:completed => true)
-  scope :incomplete, where(:completed => false)
-
 
   def self.for_list(list)
-      where(:list_id => list.id)
+    where(:list_id => list.id)
   end
 
-  # def sms_frequency_must_be_before_due_at
-  #   self.sms_frequency = self.sms_frequency.gsub!(/\D/,"")
-  #   errors.add(:sms_frequency, "too few hours until due date") if (self.due_at - Time.now) < (self.sms_frequency.to_i * 3600)
-  # end
+  def self.complete_task(task_id)
+    task = find(task_id)
+    task.completed? ? task.completed = false : task.completed = true
+    task.save
+  end
 
 end
